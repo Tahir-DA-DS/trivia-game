e9a14f364cc9b783ca64a5fc6ab1fc39a2a4a3fe
Tahir-DA-DS <tahir.adeleye@gmail.com>
2023-01-18 01:13:30 +0100

    trivia project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const http = require ("http");
+const path = require("path");
+const socketio = require("socket.io")
+const app = express();
+
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+app.use(express.static(publicDirectoryPath));
+
+const port = process.env.PORT || 8080;
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', () => { // listen for new connections to Socket.IO
+  console.log('A new player just connected');
+  })
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}.`);
+});
